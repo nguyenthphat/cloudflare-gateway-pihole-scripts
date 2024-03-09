92a16da9673692d5bbc6fb9a14dd4ab0776cfbca
nguyenthphat <75682845+nguyenthphat@users.noreply.github.com>
2024-03-09 10:19:22 +0700

    Update get_recommended_whitelist.sh

--- get_recommended_whitelist.sh
+++ get_recommended_whitelist.sh
@@ -1,35 +1,3 @@
 #!/bin/bash
-#  
-# Use the provided lists or add you own
-# https://oisd.nl/includedlists/whitelists
-# by creating an empty whitelist.csv file
-touch whitelist.csv
 
-# declare an array of urls
-urls=(
-    #https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/banks.txt
-    #https://raw.githubusercontent.com/AdguardTeam/AdGuardSDNSFilter/master/Filters/exclusions.txt
-    #https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/windows.txt
-    https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/android.txt
-    #https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/sensitive.txt
-    https://raw.githubusercontent.com/AdguardTeam/HttpsExclusions/master/exclusions/firefox.txt
-    #personal whitelist for VPN
-    https://raw.githubusercontent.com/nguyenthphat/Cloudflare-Zero-Trusted-domain-lists/main/whitelist.txt
-
-)
-
-# loop through the urls and download each file with curl
-for url in "${urls[@]}"; do
-  # get the file name from the url
-  file=$(basename "$url")
-  # download the file with curl and save it as file.txt
-  curl -o "$file.txt" "$url"
-  # append the file contents to whitelist.csv and add a newline
-  cat "$file.txt" >> whitelist.csv
-  echo "" >> whitelist.csv
-  # remove the file.txt
-  rm "$file.txt"
-done
-
-# print a message when done
-echo "Done. The whitelist.csv file contains merged data from recommended whitelists."
+node download_lists.js blocklist
